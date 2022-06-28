dc3e37b8bc885c30199a11caea41c5fbf1964251
Artem <super.alshan@mail.ru>
2022-06-28 17:05:07 +0300

    Update schema.sql
    Add new fields

--- schema.sql
+++ schema.sql
@@ -4,6 +4,12 @@ id INTEGER PRIMARY KEY,
 
 title TEXT NOT NULL,
 
-resolution TEXT NOT NULL
+resolution TEXT NOT NULL,
+
+name_file TEXT NOT NULL,
+
+datetime INTEGER NOT NULL,
+
+UNIQUE ("title") ON CONFLICT IGNORE
 
 );
